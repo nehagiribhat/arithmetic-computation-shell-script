b932ab1431c600c38d1a5d0a965e59cf9300b841
nehagiribhat <nehagiribhat@gmail.com>
2020-07-19 22:37:54 +0530

    Forth Arithmetic Computation

--- arithCompSort.sh
+++ arithCompSort.sh
@@ -5,4 +5,5 @@ echo "$a $b $c"
 comp1=$((a+b*c))
 comp2=$((a*b+c))
 comp3=$((c+a/b))
-echo "1st result=$comp1, 2nd Result=$comp2, 3rd Result=$comp3"
+comp4=$((a%b+c))
+echo "1st result=$comp1, 2nd Result=$comp2, 3rd Result=$comp3, 4th Result=$comp4"
