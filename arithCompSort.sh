4197b6fc259743cb45997a3e57fda3a6ff1dd00d
nehagiribhat <nehagiribhat@gmail.com>
2020-07-19 22:33:42 +0530

    Third Arithmetic Computation

--- arithCompSort.sh
+++ arithCompSort.sh
@@ -4,4 +4,5 @@ echo "$a $b $c"
 
 comp1=$((a+b*c))
 comp2=$((a*b+c))
-echo "1st result = $comp1, 2nd Result = $comp2"
+comp3=$((c+a/b))
+echo "1st result=$comp1, 2nd Result=$comp2, 3rd Result=$comp3"
