368e52ed0c1bc407f40a32604ae3b9f714a10746
nehagiribhat <nehagiribhat@gmail.com>
2020-07-19 22:45:19 +0530

    ADDED: Storing Every Computation in Dictionary

--- arithCompSort.sh
+++ arithCompSort.sh
@@ -2,8 +2,18 @@
 read -p "Enter 3 input numbers: " a b c
 echo "$a $b $c"
 
+declare -A computation
+
 comp1=$((a+b*c))
 comp2=$((a*b+c))
 comp3=$((c+a/b))
 comp4=$((a%b+c))
 echo "1st result=$comp1, 2nd Result=$comp2, 3rd Result=$comp3, 4th Result=$comp4"
+
+computation[a+b*c]=$comp1
+computation[a*b+c]=$comp2
+computation[c+a/b]=$comp3
+computation[a%b+c]=$comp4
+
+echo "expression = ${!computation[@]}"
+echo "results = ${computation[@]}"
